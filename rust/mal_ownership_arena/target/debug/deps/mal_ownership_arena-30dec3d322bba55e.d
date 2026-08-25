2245f46acc4887506e607d6dc95850cd7f524b00
elhamdolillah <5.9179022e+07+elhamdolillah@users.noreply.github.com>
2026-08-25 01:41:56 +0000

    chore(mal): ignore Rust build artifacts

--- rust/mal_ownership_arena/target/debug/deps/mal_ownership_arena-30dec3d322bba55e.d
+++
@@ -1,7 +0,0 @@
-/home/ubuntu/uori-mal-pr/rust/mal_ownership_arena/target/debug/deps/libmal_ownership_arena-30dec3d322bba55e.rmeta: src/lib.rs
-
-/home/ubuntu/uori-mal-pr/rust/mal_ownership_arena/target/debug/deps/libmal_ownership_arena-30dec3d322bba55e.rlib: src/lib.rs
-
-/home/ubuntu/uori-mal-pr/rust/mal_ownership_arena/target/debug/deps/mal_ownership_arena-30dec3d322bba55e.d: src/lib.rs
-
-src/lib.rs:
--- rust/mal_ownership_arena/target/debug/deps/mal_ownership_arena-47cad0aadc5d841e.d
+++
@@ -1,5 +0,0 @@
-/home/ubuntu/uori-mal-pr/rust/mal_ownership_arena/target/debug/deps/mal_ownership_arena-47cad0aadc5d841e: src/lib.rs
-
-/home/ubuntu/uori-mal-pr/rust/mal_ownership_arena/target/debug/deps/mal_ownership_arena-47cad0aadc5d841e.d: src/lib.rs
-
-src/lib.rs:
